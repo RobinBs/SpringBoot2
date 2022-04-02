36e073479acd1d717e0a8abe28b917b68ba7ce60
RobinBs <baishuo9715@126.com>
2022-04-02 10:05:04 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        // 1. 获取IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
+        // 2. 打印初始化的各类对象名称
+        String[] names = run.getBeanDefinitionNames();
+        for (String beanName : names) {
+            System.out.println(beanName);
+        }
+    }
+
+}
